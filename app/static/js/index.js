a9bb019a5f647c28c546b31290cd315f385a956b
Doug Jones <doug@dfjones.net>
2012-10-21 23:11:25 -0400

    Better join logic.

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
+  var joinChannel = function(channel) {
+    window.location = "/f/" + channel;
+  };
+  var submitJoin = function () {
+    joinChannel($("#join-modal input").val());
+  };
   $("#btn-join").click(function () {
     $("#join-modal").modal();
     $("#join-modal .btn-primary").click(function () {
-      window.location = "/f/" + $("#join-modal input").val();
+      submitJoin();
+    });
+    $("#join-modal input").keypress(function (e) {
+      if (e.which == 10 || e.which == 13) {
+        submitJoin();
+      }
     });
   });
 });
